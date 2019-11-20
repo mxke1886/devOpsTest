b0812d753ced21c1036f20a515d7487754b7ee3c
mxke1886 <mike.meier12.mm@gmail.com>
2019-11-20 15:35:12 +0100

    Add files via upload

---
+++ src/main/java/ch/noser/learningDevOps/HelloWorldController.java
@@ -0,0 +1,13 @@
+package ch.noser.learningDevOps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/ch/noser/learningDevOps/HelloWorldControllerTest.java
@@ -0,0 +1,14 @@
+package ch.noser.learningDevOps;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldControllerTest {
+
+    @Test
+    void helloWorld() {
+        assertThat(1).isEqualTo(1);
+    }
+}
